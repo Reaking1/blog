e272846cc3e7015a6965aebb3043b30b5cb8e873
Reaking1@ <reabetsweramoswane@gmail.com>
2023-11-22 21:55:50 +0200

    update from last

--- src/componets/pages/Blog/Section4.js
+++ src/componets/pages/Blog/Section4.js
@@ -1,9 +1,44 @@
-import { faHourglass1 } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import './index.scss'
 
 const Section4 = () => {
    return  (
-    <h1>Under Construction <FontAwesomeIcon icon={faHourglass1} beatFade idx={20}/></h1>
+    <div className="sevtion4 container">
+      <h1>The Art of Woodworking: Crafting Masterpieces from Scratch</h1>
+      <div className="section4 text">
+         <p>Embark on a journey into the world of woodworking, where raw materials transform into functional and aesthetically pleasing creations. Learn the basics of selecting wood, mastering essential tools, and creating everything from intricate furniture pieces to simple yet elegant home decor.
+         Woodworking is a hobby that combines practical skills with artistic expression. Engaging in woodworking allows individuals to create functional items from scratch, developing craftsmanship and problem-solving abilities. Whether it's crafting furniture, decorative pieces, or even small household items, woodworking provides a tangible outlet for creativity while simultaneously enhancing manual dexterity and attention to detail.
+         </p>
+      </div>
+      <h1>Capturing Moments Through the Lens: A Photography Exploration</h1>
+      <div className="section4 text">
+      <p>Unleash your creativity and explore the captivating realm of photography. From mastering camera settings to understanding composition, this hobby guide will help you develop your skills. Discover the joy of freezing moments in time and expressing your unique perspective through the art of photography.
+      Photography is a powerful hobby for capturing moments that matter. By mastering the art of photography, individuals can freeze significant moments in time, creating a visual chronicle of their lives. Whether it's documenting everyday scenes, special occasions, or exploring the beauty of the world, photography not only sparks creativity but also becomes a means to preserve cherished memories for years to come.
+      </p>
+      </div>
+      <h1>Guitar Playing: Expressing Emotions Through Music</h1>
+      <div className="section4 text">
+         <p>
+         Dive into the world of music by learning to play the guitar. This hobby guide takes you through the basics of choosing the right instrument, understanding chords, and mastering strumming patterns. Whether you're into folk, rock, or blues, discover the joy of creating music with your own hands
+         Learning to play the guitar is a hobby that provides a unique form of self-expression. Music has the ability to convey a wide range of emotions, and by mastering the guitar, individuals can translate their feelings into melodies. Whether strumming chords for personal enjoyment or performing for others, playing the guitar fosters a deeper connection to the art of sound and allows for a form of emotional expression that words alone cannot capture.
+         </p>
+      </div>
+      <h1>Ink and Imagination: The Art of Sketching and Drawing</h1>
+      <div className="section4 text">
+         <p>Awaken your artistic side with the soothing strokes of a pencil. This guide to sketching and drawing covers fundamental techniques, shading, and perspective. Uncover the joy of bringing your imagination to life on paper, whether it's sketching everyday scenes or creating fantastical worlds.
+         Sketching and drawing serve as a creative outlet that transcends language barriers. This hobby allows individuals to bring their imagination to life on paper, fostering artistic expression and visual communication. It promotes mindfulness and self-discovery while developing hand-eye coordination. Sketching and drawing are not just about creating art; they are about exploring one's own imagination and unique perspective on the world.
+         </p>
+      </div>
+      <h1>Gardening: Nurturing Life and Creating Tranquility</h1>
+      <div className="section4 text">
+         <p>Step into the world of gardening, where patience and care bloom into vibrant landscapes. Learn the basics of selecting plants, understanding soil, and nurturing your garden. This hobby guide invites you to create a tranquil oasis, whether you have a backyard or just a few pots on a balcony.
+         Gardening is a hobby that goes beyond the physical act of planting seeds. It involves nurturing life, observing growth, and creating spaces that bring tranquility. Gardening provides a connection to nature, promoting relaxation and stress relief. It instills a sense of responsibility and patience as individuals witness the gradual transformations of their plants. Beyond the beauty of blossoms, gardening becomes a metaphor for cultivating a peaceful and balanced life.
+         </p>
+      </div>
+      <h1>Stargazing: Exploring the Wonders of the Universe</h1>
+      <div className="section4 text">
+         <p>Explore the cosmos from the comfort of your backyard with this beginner's guide to stargazing. Learn to identify constellations, understand celestial events, and even choose your first telescope. Join the community of sky watchers and embark on a journey through the awe-inspiring beauty of the night sky.Stargazing is a hobby that invites individuals to look beyond the confines of Earth and explore the wonders of the universe. It instills a sense of awe and curiosity as one gazes at the night sky, identifying constellations, planets, and celestial events. Stargazing connects individuals to the vastness of space, inspiring contemplation and expanding their understanding of the cosmos. It is a hobby that fosters a sense of humility and encourages a lifelong pursuit of knowledge.</p>
+      </div>
+    </div>
    )
 }
 
--- src/componets/pages/Blog/Section5.js
+++ src/componets/pages/Blog/Section5.js
@@ -1,11 +1,33 @@
-import { faHourglass1 } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import './index.scss'
 
 
 const Section5 = () => {
 
     return  (
-        <h1>Under Construction <FontAwesomeIcon icon={faHourglass1} beatFade idx={20}/></h1>
+       <div className="section5 container">
+        <h1>Becoming the Top 1% Developer: A Journey of Skill, Persistence, and Growth</h1>
+        <div className='section5 text'>
+            <p>
+            In the fast-paced realm of technology, aspiring to be in the top 1% of developers demands more than just technical prowess; it requires a holistic approach encompassing continuous learning, adaptability, and strategic career moves.
+
+Firstly, a solid foundation in core technologies is paramount. Master the languages and frameworks relevant to your desired field. Specialization is key—become an expert rather than a jack of all trades. Delve deep into areas like web development, data science, or mobile app development, aligning your skills with industry demands.
+
+Equally crucial is staying abreast of industry trends. Technologies evolve swiftly, and staying relevant is non-negotiable. Follow blogs, participate in online forums, and attend conferences. Networking is not only about making connections; it's about absorbing knowledge and staying ahead in an ever-evolving landscape
+            </p>
+            <p>
+            Success in tech is not solely about coding. Cultivate soft skills. Effective communication, problem-solving, and teamwork are invaluable. Demonstrate your ability to translate complex technical concepts into plain language, a skill highly prized in collaborative work environments.
+
+Strategic project choices can set you apart. Contribute to open-source projects, build a portfolio showcasing your best work, and engage in side projects to display versatility. Demonstrate a passion for innovation and problem-solving. Hiring managers look for real-world applications of skills, not just theoretical knowledge.
+
+Entering the tech industry often begins with internships or entry-level positions. Embrace these opportunities wholeheartedly. Treat every project as a chance to learn, and every challenge as an opportunity to grow. Establishing a strong work ethic and a reputation for dependability in these early stages lays the groundwork for future success.
+                </p>
+                <p>
+                Above all, embrace a growth mindset. Recognize that the journey to becoming a top-tier developer is ongoing. Seek feedback, learn from failures, and constantly refine your skills. Adaptability, coupled with a commitment to lifelong learning, is the secret sauce that propels developers into the echelons of the top 1%.
+
+In conclusion, to be the 1% developer, one must be more than just technically proficient. It requires a strategic, well-rounded approach—mastering core technologies, staying informed, developing soft skills, making strategic project choices, seizing opportunities, and maintaining a growth mindset. The journey is challenging, but the rewards are immeasurable.
+                </p>
+        </div>
+       </div>
        )
 }
 
