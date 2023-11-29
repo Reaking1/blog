4acc93f45542c36b64fa09e5bef2a5963d3b2006
Reaking1@ <reabetsweramoswane@gmail.com>
2023-11-29 03:39:15 +0200

    updated

--- src/componets/Layouts/RootLayout.js
+++ src/componets/Layouts/RootLayout.js
@@ -1,26 +1,25 @@
-import { NavLink, Outlet } from "react-router-dom"
-import '../../App.scss'
-import {Analytics} from '@vercel/analytics'
-
-
+import { NavLink, Outlet } from "react-router-dom";
+import '../../App.scss';
+import { Analytics } from '@vercel/analytics';
 
 const RootLayout = () => {
-   return (
+  return (
     <div className="rootLayout">
-        <header>
-         <nav>
-            <NavLink to="/" >Home</NavLink>
-            <NavLink to="/blog" >Blog</NavLink>
-            <NavLink to="/about">About</NavLink>
-           
-         </nav>
-        </header>
-       <main>
-         <Analytics />
-         <Outlet />
-       </main>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Analytics id="prj_GTxqaX77Ie8jdUCOCmOWsU9dcceQ">
+          {/* Optional: Any content you want to track */}
+          <Outlet />
+        </Analytics>
+      </main>
     </div>
-   )
- }
- 
- export default RootLayout
+  );
+};
+
+export default RootLayout;
